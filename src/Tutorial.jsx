d821addd736195fa5f7283b7f458156c0d04a960
ibnuakbar824 <162837713+ibnuakbar824@users.noreply.github.com>
2024-03-10 16:09:08 +0700

    Pertemuan 1

---
+++ src/Tutorial.jsx
@@ -0,0 +1,13 @@
+export default function Tutorial(){
+    return(
+        <>
+        <h1>Tutorial</h1>
+        <p>Tuturial mudah</p>
+        <p>Belajar Mudah</p>
+
+        <div>
+            <h3 style={{color: 'red'}}>Belajar</h3>
+        </div>
+    </>
+    )
+}
